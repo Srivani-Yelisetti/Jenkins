7e8ecd6e8bcffa6bdb5dce94d44b75fdf9252236
Srivani-Yelisetti <102016996+Srivani-Yelisetti@users.noreply.github.com>
2022-04-10 10:48:44 +0530

    Update text.sh

--- text.sh
+++ text.sh
@@ -1,5 +1,5 @@
-sudo apt-get update
-sudo apt-get install \
+apt-get update
+apt-get install \
     ca-certificates \
     curl \
     gnupg \
@@ -8,5 +8,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io -y
